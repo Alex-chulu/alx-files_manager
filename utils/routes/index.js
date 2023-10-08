d5b947091ee530796323a7750e997c5c932cb0d5
Alex-chulu <chulualexander@gmail.com>
2023-10-08 08:48:00 +0000

    task 2 fix

--- utils/routes/index.js
+++ utils/routes/index.js
@@ -1,10 +1,20 @@
-const express = require('express');
-const router = express.Router();
-const AppController = require('../controllers/AppController');
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+
+const router = Router();
 
 // Define routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-module.exports = router;
+// Routes for users
+router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
 
+// authentication of routes
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+
+export default router;
