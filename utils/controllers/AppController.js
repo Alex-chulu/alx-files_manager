d7c6ea6bc608ded55a98ede94ec4781ccfd935e2
Alex-chulu <chulualexander@gmail.com>
2023-10-08 08:53:52 +0000

    task 2 fix

--- utils/controllers/AppController.js
+++ utils/controllers/AppController.js
@@ -1,32 +1,22 @@
-// Import necessary modules and models
-const mongoose = require('mongoose');
-const User = require('../models/User'); // Import your user model
-const File = require('../models/File'); // Import your file model
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
-const AppController = {
-  getStatus: async (req, res) => {
-    // Check Redis and DB status here
-    const redisStatus = true; // Replace with your Redis status check
-    const dbStatus = mongoose.connection.readyState === 1; // 1 indicates connected to DB
+function getStatus(req, res) {
+  const status = {
+    redis: redisClient.isAlive(),
+    db: dbClient.isAlive(),
+  };
+  res.status(200).json(status);
+}
 
-    res.status(200).json({ redis: redisStatus, db: dbStatus });
-  },
+async function getStats(req, res) {
+  const stats = {
+    users: await dbClient.nbUsers(),
+    files: await dbClient.nbUsers(),
+  };
+  res.status(200).json(stats);
+}
 
-  getStats: async (req, res) => {
-    try {
-      // Fetch user count from the User model
-      const userCount = await User.countDocuments();
-
-      // Fetch file count from the File model
-      const fileCount = await File.countDocuments();
-
-      res.status(200).json({ users: userCount, files: fileCount });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  },
+module.exports = {
+  getStats, getStatus,
 };
-
-module.exports = AppController;
-
